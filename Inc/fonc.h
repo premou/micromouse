82ffd6f516a8a9898d7f0cad4f3d75db8bd431fd
pat92fr <48148915+pat92fr@users.noreply.github.com>
2019-03-06 18:50:00 +0100

    [FONC] clean-up
    unused source code deleted

--- Inc/fonc.h
+++ Inc/fonc.h
@@ -11,29 +11,5 @@
 #include "main.h"
 #include "stm32f7xx_hal.h"
 
-/*
-void marche_avant(uint32_t speed);
-void marche_arriere(uint32_t speed);
-void arret();
-*/
 
-
-
-
-void run(int32_t speed_right, int32_t speed_left);
-
-
-void init_setpoint();
-
-/**
-
-
-/**
- * Return la valeur de la vitesse en m/s
- * dist : la distance parcourue en m
- * time : le temps de parcours en ms
- */
-float get_speed(float dist, uint32_t time);
-
-uint32_t get_setpoint(float t, float * vitesse);
 #endif /* FONC_H_ */
