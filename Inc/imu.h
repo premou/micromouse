604eb70214a997a5c554d47942fc4dd00126daeb
pat92fr <48148915+pat92fr@users.noreply.github.com>
2019-03-11 23:19:29 +0100

    [IMU] GYRO source code example added

---
+++ Inc/imu.h
@@ -0,0 +1,54 @@
+/*
+ * imu.h
+ *
+ *  Created on: 25 déc. 2015
+ *      Author: Patrick
+ */
+
+#ifndef APPLICATION_USER_HAL_IMU_H_
+#define APPLICATION_USER_HAL_IMU_H_
+
+#include "stm32f7xx_hal.h"
+#include <stdbool.h>
+
+
+/**
+  * @brief  IMU Handler Structure definition
+  */
+typedef struct {
+	I2C_HandleTypeDef * hi2c;		/*!< Specifies the GPIO pin used to generate STEP/PULSE signal. */
+	// Acquisition Data
+	float * sensitivity_correction; // 1.0 default
+	int16_t raw_rate; 			// digit
+	float scaled_rate; 		// dps
+	// Bias Correction
+	float rate_mean; 		// Moyenne du gyroscope
+	float rate_qmean;		// Moyenne quadratique du gyroscope
+	float rate_deviation; 	// Variance du gyroscope
+	float rate_bias;
+	// User data
+	float rate; // dps
+	double heading; // degres
+} IMU_HandleTypeDef;
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+void HAL_IMU_Init();
+
+void HAL_IMU_Add(
+		IMU_HandleTypeDef * himu,
+		I2C_HandleTypeDef * hi2c,
+		float * sensitivity_correction
+);
+
+void HAL_IMU_Read_Sensors(IMU_HandleTypeDef * himu, bool autocalibrate, float period_us);
+
+void HAL_IMU_Zero_Heading(IMU_HandleTypeDef * himu);
+
+#ifdef __cplusplus
+}
+#endif
+
+ #endif /* APPLICATION_USER_HAL_IMU_H_ */
