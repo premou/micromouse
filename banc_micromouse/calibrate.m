37bae1f6a2f8c7377c38baac70963130c0f042b1
pat92fr <48148915+pat92fr@users.noreply.github.com>
2019-04-24 08:20:49 +0200

    [BancMicromouse] MATLAB update

--- banc_micromouse/calibrate.m
+++ banc_micromouse/calibrate.m
@@ -6,10 +6,10 @@
 % ? with the target surface :
 % s(x, ? ) = (? / x^2) cos ? + ?
 
-% with Bernard, 1/sqrt(ADC) is almost LINER
+% with Bernard, 1/sqrt(ADC) is almost LINEAR
 % y = 1/(ax+b)^2
 % x = (1/sqrt(y)-b)/a
-
+close all
 clear
 pkg load image
 % cd ....
@@ -19,14 +19,17 @@ distance = data([2:180],1);
 raw = [ data([2:180],2), data([2:180],3), data([2:180],4), data([2:180],5) ];
 % median filter on raw data
 filt_raw = medfilt2(raw,[5 1]);
-figure(1);
+%figure(1);
+figure(1,"position",[0,0,1920,1040])
+subplot(2,3,1)
 plot(filt_raw)
 legend("DL","FL","FR","DR")
 title("distance (mm) to filtered ADC (12bits)")
-print -dpng raw
+%print -dpng raw
 % find a (ln or squareroot law)
 log_raw = 1./sqrt(filt_raw);
-figure(2);
+%figure(2);
+subplot(2,3,2)
 plot(log_raw)
 legend("DL","FL","FR","DR")
 title("distance (mm) to 1/sqrt(ADC) [linearisation]")
@@ -34,7 +37,8 @@ A = zeros(40,4);
 for index = [1:40]
   A(index,:) = (log_raw(index+30+20,:) - log_raw(index+30,:))/20;
 end;
-figure(3);
+%figure(3);
+subplot(2,3,3)
 plot(A)
 legend("DL","FL","FR","DR")
 title("distance [30,70] (mm) to 'a' parameter ")
@@ -43,23 +47,34 @@ a = mean(A,1)
 b = log_raw(50,:)-50.*a
 % final
 synthetic_data = [ 1./((a(1)*distance+b(1)).^2), 1./((a(2)*distance+b(2)).^2),  1./((a(3)*distance+b(3)).^2),  1./((a(4)*distance+b(4)).^2) ];
-figure(4);
+%figure(4);
+subplot(2,3,4)
 plot(synthetic_data)
 legend("DL","FL","FR","DR")
 title("distance (mm) to ADC recalculated using 'a' and 'b' parameters with formula y=1/(ax+b)^2")
 % error
 synthetic_distance= [ (1./sqrt(filt_raw(:,1))-b(1))/a(1), (1./sqrt(filt_raw(:,2))-b(2))/a(2), (1./sqrt(filt_raw(:,3))-b(3))/a(3),  (1./sqrt(filt_raw(:,4))-b(4))/a(4)];
 error = synthetic_distance.-distance;
-figure(5);
+%figure(5);
+subplot(2,3,5)
+distance_reference = [2:1:180];
 plot(synthetic_distance)
-legend("DL","FL","FR","DR")
+hold on;
+plot(distance_reference,":k")
+legend("DL","FL","FR","DR", "REF")
 title("distance (mm) to recalculated distance from ADC using 'a' and 'b' parameters with x=(1/sqrt(y)-b)/a")
-print -dpng recalculated_distance 
-figure(6);
-plot(error)
-legend("DL","FL","FR","DR")
+%print -dpng recalculated_distance 
+%figure(6);
+subplot(2,3,6)
+error_reference = zeros(179,1);
+%plot([error,error_reference])
+plot(error);
+hold on;
+plot(error_reference,":k")
+legend("DL","FL","FR","DR","REF")
 title("distance (mm) to distance error (mm) from ADC using 'a' and 'b' paramters")
-print -dpng error 
+%print -dpng error 
 % save results
 save params.txt a b
+print -dpng results 
 
