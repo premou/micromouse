8d46044581d02c136fa1d67a6548c855e0d68b4a
pat92fr <48148915+pat92fr@users.noreply.github.com>
2019-04-27 12:52:18 +0200

    [BancMicromouse] Remplacement GradientDescent par Normalize (meilleure précision)
    Approximation polynomiale en 1 + 1/racine(ADC) + 1/carre(ADC)
    Solution en [theta0 theta1 theta2]
    Production des paramètres  de tous les capteurs

---
+++ banc_micromouse/NORMcalibrate.m
@@ -0,0 +1,55 @@
+% machine learning
+%% data file order "DL"(2) ,"FL"(3) ,"FR"(4), "DR"(5)
+name = [ "DL"; "FL"; "FR"; "DR" ];
+
+%% initialization
+clear ; close all; clc ;
+
+%% load data set
+load data.txt
+
+%% size of data set (without first set)
+m = size(data,1)-1;
+
+%% build data set y
+y = data(2:m+1,1);
+
+for sensor = 1:4
+  %% build data set X 
+  rawX = data(2:m+1,sensor+1); %% SELECT DATA SET : "DL"(2) ,"FL"(3) ,"FR"(4), "DR"(5)
+
+  %% one feature with polynomial regression x <=> 1/sqrt(ADC) 1/ADC 1/ADC²
+  %% add x0 col
+  %X = [ ones(m,1), 1./sqrt(rawX), 1./rawX, 1./(rawX.^2) ];
+  X = [ ones(m,1), 1./sqrt(rawX), 1./(rawX.^2) ];
+  %X = [ ones(m,1), 1./sqrt(rawX), 1./(rawX) ];
+
+  %% theta
+  theta = pinv(X'*X)*X'*y
+
+  #error over distance
+  error = X*theta - y;
+
+  % plot x (ADC) => y (distance)
+  subplot(4,2,2*sensor-1)
+  plot(rawX,y);
+  hold on;
+  plot(rawX, X*theta);
+  xlabel("ADC");
+  ylabel("Distance(mm)");
+  %legend("Raw measures","H")
+  title(sprintf("Raw ADC value to Distance %s", { "DL"; "FL"; "FR"; "DR" }{sensor}))
+  subplot(4,2,2*sensor)
+  plot(y(20:175),error(20:175),"r");
+  hold on;
+  plot(y,zeros(m,1),'k');
+  xlabel("Distance(mm)");
+  ylabel("Error(mm)");
+  %legend("H","reference")
+  title("Error")
+
+  % save
+  filename = sprintf("hypothesis_%s.txt", { "DL"; "FL"; "FR"; "DR" }{sensor})
+  save(filename, "theta")
+  print -dpng NORMresults
+end
